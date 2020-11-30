8dd37cfba48ff192a42827ce860de1d0b386e58d
„Weronika <weronika.tabakowska@student.wat.edu.pl>
2020-11-30 11:53:36 +0100

    Wykonano zadanie 2.2.3 – wskaźniki

--- Zadanie 2.3/src/main.cpp
+++ Zadanie 2.3/src/main.cpp
@@ -1,22 +1,42 @@
-#include <Arduino.h>
-volatile int state = LOW;
+#include <avr/io.h>
 
-int main() {
-  init();
-  DDRB |= B00110000;
-  PORTD |= (1 << PORTD2);
-  EICRA |= (1 << ISC00);
-  EIMSK |= (1 << INT0);
-  sei();
-  while (1) {
-    PORTB |= (1<< PORTB5);
-    delay(500);
-    PORTB &= !(1<< PORTB5);
-    delay(500);
-  }
+uint8_t ledState[] = {0xFF, 0x7E, 0x3C, 0x18, 0x00, 0x18, 0x3C, 0x7E};
+uint8_t *pLedState = ledState;
+bool state = true;
+
+void togglePinD13(bool *state)
+{
+  PORTB = (*state << 5);
+  *state = !(*state);
 }
 
-ISR(INTO_vect){
-  state = !state;
-  digitalWrite(12, state);
+void delay()
+{
+  for (uint32_t j = 0x2FFFF; j > 0; j--)
+{
+  __asm__ __volatile__("nop");
+}
+}
+
+int main()
+{
+  DDRB |= (1 << 5);
+  DDRD |=0xFF;
+  while (1)
+  {
+    togglePinD13(&state);
+    delay();
+    for (uint8_t i = 0; i < sizeof(ledState); i++)
+    {
+      PORTD = ledState[i];
+      delay();
+    }
+    pLedState = ledState;
+    for (uint8_t i = 0; i < sizeof(ledState); i++)
+    {
+      PORTD = *pLedState;
+      pLedState++;
+      delay();
+    }
+  }
 }
